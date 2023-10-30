2a5f10ec83790f40020d708185188e24b41a8e54
Sergenti <florian.rieder@unil.ch>
2023-10-30 18:53:18 +0100

    Removed description field from daily item controller

--- objetslitt/controllers/daily_item.rb
+++ objetslitt/controllers/daily_item.rb
@@ -20,7 +20,6 @@ module DailyItem
 
     # Find fields UUIDS
     nom = objet_type.find_field("nom").uuid
-    descripfct = objet_type.find_field("descripfct").uuid
     cover = objet_type.find_field("cover").uuid
 
     # Extract the necessary attributes from the random item
@@ -29,12 +28,6 @@ module DailyItem
     cover_path = random_item.data[cover]&.dig("path")
     data[:cover] = cover_path ? "/" + cover_path : nil
     data[:url] = "/objetslitt/#{I18n.locale}/objets/#{random_item.id}"
-    # descripfct is an optional field. Therefore, check if it's present
-    if random_item.data.key?(descripfct) && !random_item.data[descripfct].empty?
-      data[:description] = JSON.parse(random_item.data[descripfct])["content"]
-    else
-      data[:description] = "Description indisponible."
-    end
 
     data
   end
