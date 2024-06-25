857fb04da203a1aed8626817c67e68e1bf5f0a77
Luca Guindani <luca.guindani@gmail.com>
2024-06-25 10:36:18 +0200

    Fix Viatimages advanced search with custom catalog issue Update Viatimages advanced search css

--- viatimages/controllers/viatimages_advanced_searches_controller.rb
+++ viatimages/controllers/viatimages_advanced_searches_controller.rb
@@ -7,7 +7,7 @@ class ViatimagesAdvancedSearchesController < AdvancedSearchesController
     # Sort the advance search configurations by slug to always have the
     # "image" configuration as the first one.
     @advance_search_confs = @advance_search_confs.to_a.sort_by { |conf|
-      conf.slug
+      conf.slug || ''
     }.reverse
   end
 
