138ae5278c060c3ba35d1b50fb48b200ffa5503a
Luca Guindani <luca.guindani@gmail.com>
2024-10-15 10:09:28 +0200

    Add corpus results to Viatimages simple searches Update Viatimages footer logos

--- viatimages/controllers/viatimages_simple_searches_controller.rb
+++ viatimages/controllers/viatimages_simple_searches_controller.rb
@@ -4,14 +4,26 @@ class ViatimagesSimpleSearchesController < SimpleSearchesController
   def show
     super
 
-    # Restrict the search context to the images item type
-    @simple_search_results = ItemList::SimpleSearchResult.new(
-      :catalog => catalog,
-      :query => @saved_search.query,
-      :page => params[:page],
-      :item_type_slug => 'images',
-      :search_uuid => @saved_search.uuid
-    )
+    # If no item type is specified, we want to show the first available item type
+    # that has results. We want to restrict the search context to the images or corpus
+    # item types. We will loop through the item types and break when we find
+    # the first item type.
+    #
+    # We do all of this because we don't want to show any other item types in the
+    # search results, and we want to show the images tab first if available.
+    unless params[:type]
+      @simple_search_results.item_counts_by_type.sort_by { |type, _| type.slug == 'images' ? 0 : 1 }.each do |type, count|
+        item_type_slug = type.slug == 'images' || type.slug == 'corpus' ? type.slug : 'cannotwork'
+        @simple_search_results = ItemList::SimpleSearchResult.new(
+          catalog: catalog,
+          query: @saved_search.query,
+          page: params[:page],
+          item_type_slug: item_type_slug,
+          search_uuid: @saved_search.uuid
+        )
+        break
+      end
+    end
 
     # Retrieve the default advanced search configuration
     # to show the advanced search link in the view
